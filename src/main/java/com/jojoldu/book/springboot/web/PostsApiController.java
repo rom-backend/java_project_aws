4c7bb346a2d87b750de80489243c3c64e5cfd85c
rom <alterin@naver.com>
2021-06-10 19:45:48 +0900

    jpa 적용진행중2

---
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsSaveRequestDto {
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
