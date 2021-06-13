c95420a1c7789e76db82869972cf2dd30b6b35e9
rom <alterin@naver.com>
2021-06-13 22:34:09 +0900

    test

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -30,7 +30,7 @@ public class HelloControllerTest {
 
     @Test
     public void helloDto가_리턴된다() throws Exception {
-        String name = "hello123";
+        String name = "hello";
         int amount = 1000;
 
         mvc.perform(get("/hello/dto").param("name", name)
